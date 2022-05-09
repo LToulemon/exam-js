cec717a63f85c37b3c64d681159bf41a423f368e
LTOULEMON <louis.toulemon@ynov.com>
2022-05-09 12:53:01 +0200

    jadormanipdéJSON

---
+++ extract-object.js
@@ -0,0 +1,19 @@
+function extractObject(str){
+    for (var i in str)
+        var key = []
+        if(i == key && obj[i]== val){
+            return "ptn de JSON"
+        }
+    
+    /*let res = {}
+    str =`Hello there {"hello":2,"a":3} comment ça va ? {"a":1,"z":2} {"hello":"World","abc":"def"}`
+    let fields = str.split('{')
+    var json1 = fields[0]
+    try {
+       
+    }catch (e) { 
+        console.error(e)
+    }*/
+}
+
+module.exports = extractObject
